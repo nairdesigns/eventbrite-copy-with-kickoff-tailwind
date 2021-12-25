bd44773323d3f1926cdeead03ebc985a971c6159
Nair Designs` <nairbdg@gmail.com>
2021-12-25 10:48:53 +0900

    bundle install number 2 good

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  resources :meetings
+  devise_for :users 
+  root to: 'home#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
